493ea665ac5fb066e3d65e5b082793e5a3d3248a
Srija <srijajha05@gmail.com>
2024-10-10 01:26:14 +0530

    project deploment ready done

--- backend/app.js
+++ backend/app.js
@@ -7,12 +7,16 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
 
 
 dotenv.config({});
 
-const app = express();
+
 const PORT = process.env.PORT || 3000;
+const app = express();
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -28,6 +32,11 @@ app.use("/api/v1/user",userRoute);
 app.use("/api/v1/company",companyRoute);
 app.use("/api/v1/job",jobRoute);
 app.use("/api/v1/application",applicationRoute);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+})
 
 app.listen(PORT,()=>{
     connectDB();
